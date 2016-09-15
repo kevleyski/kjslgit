65ce3ad7d2f5711b6841dcc9c8ccf62648378744
Kevin Staunton-Lambert <kevleyski@gmail.com>
2016-09-15 13:05:23 +1000

    Adding make

---
+++ bar.hpp
@@ -0,0 +1,17 @@
+//
+//  bar.hpp
+//  kjslgit
+//
+//  Created by Dad on 15/09/2016.
+//  Copyright © 2016 Kevin Staunton-Lambert. All rights reserved.
+//
+
+#ifndef bar_hpp
+#define bar_hpp
+
+#include <stdio.h>
+
+#endif /* bar_hpp */
+
+
+// added from xcode
