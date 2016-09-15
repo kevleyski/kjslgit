4ce5f0bfd5213a80de5427611d86133453ef3ab0
Kevin Staunton-Lambert <kevleyski@gmail.com>
2016-09-15 13:35:35 +1000

    Scratch pad

---
+++ CityMatrix.h
@@ -0,0 +1,37 @@
+/*
+ * NeighbourhoodMatrix.h
+ *
+ *  Created on: 28Aug.,2016
+ *      Author: klambert
+ */
+
+#ifndef CITYMATRIX_H_
+#define CITYMATRIX_H_
+
+/* 5x5 Matrix, example two neighbourhoods at level 3
+ *
+ *     |01234
+ *    -------
+ *    0|44444
+ *    1|44334
+ *    2|44434
+ *    3|44344
+ *    4|44334
+ */
+
+namespace Donn {
+
+class CityMatrix {
+
+  std::vector<std::vector<int> > m_matrix;
+
+ public:
+  CityMatrix();
+  virtual ~CityMatrix();
+  friend std::ostream& operator<<(std::ostream& os, const CityMatrix& matrix);
+  unsigned int getLowestIncome() const;
+};
+
+}
+
+#endif /* CITYMATRIX_H_ */
